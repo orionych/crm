6aaae719174bca4ad2a3b6bfe78befc1d3812aeb
= <=>
2017-01-11 12:43:38 +0300

    with vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,15 +6,15 @@
  */
 
 require('./bootstrap');
-
+// var Vue=require('vue');
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('firm', require('./components/Firm.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: 'body'
 });
