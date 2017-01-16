df9f3fdf2e27052882698b0be1f1df7c609646a0
= <=>
2017-01-16 23:50:40 +0300

    new one

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\User;
 	Route::get('/', function() {
 		$users=User::all();
 		foreach ($users as $user)
-			{echo $user->permissions->where('name','user_admin')->exists();}
+			{echo $user->permissions->where('name','user_admin');}
 		return view('welcome');});
 	Route::get('/someUrl', function() {return 'Super CRM system '.csrf_token().' ';});
 	//Route::get('/someUrl', function() {return Response::error('404');});
