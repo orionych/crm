36333f2e0645592a6bab5179e50148f2fde40c69
= <=>
2017-01-16 23:11:16 +0300

    with role

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
 
 
     $this->call(OldUsersSeeder::class);
+    $this->call(RoleSeeder::class);
 
         // $this->call(UsersTableSeeder::class);
     }
---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Permission;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       Permission::create(['name'=>'user_admin']);
+    }
+}
