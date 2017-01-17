747b030d41c729fcc8e2d1a044a018ce7dbcc308
= <=>
2017-01-17 15:16:11 +0300

    Auto-committing 'layout.blade.php'

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,6 +25,9 @@
               <div class="sidebar-toggle-box">
                   <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
               </div>
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
             <!--logo start-->
             <a href="index.html" class="logo"><b>LEAD<span>ER</span></b></a>
             <!--logo end-->  
