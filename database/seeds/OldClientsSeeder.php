be92356a978c7332c8c2aa7c090694a5f9cc08da
= <=>
2017-01-17 23:28:34 +0300

    Auto-committing 'OldClientsSeeder.php'

---
+++ database/seeds/OldClientsSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\OldClient;
+use Illuminate\Database\Seeder;
+
+class OldClientsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        	$clients=OldClient::all();
+	DB::statement("SET FOREIGN_KEY_CHECKS=0");
+	DB::table('clients')->truncate();
+   	DB::statement("SET FOREIGN_KEY_CHECKS=1");
+    	Eloquent::unguard();    
+    }
+}
