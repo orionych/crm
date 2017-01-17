0f7a03184c12476abf86b41786877e332e0070e7
= <=>
2017-01-17 23:38:24 +0300

    Auto-committing '2017_01_17_203412_create_requests_table.php'

---
+++ database/migrations/2017_01_17_203412_create_requests_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type_id')->default(0);
+            $table->integer('client_id')->default(0);
+            $table->text('text');
+            $table->integer('status')->default(1);
+            $table->boolean('important')->default(false);
+            $table->integer('user_id')->default(0);
+            $table->timestamp('call_at')->
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
