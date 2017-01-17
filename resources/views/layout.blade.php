ee87cc961eb5801a2d6ac2c63d0542c6abd1b4ee
= <=>
2017-01-17 15:57:28 +0300

    Auto-committing 'layout.blade.php'

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -239,7 +239,7 @@
               
 
                   <p class="centered">{{ Auth::user()->getAvatar() }}</p>
-                  <h5 class="centered">{{ Auth::user()->name }}</h5>
+                  <h5 class="centered">{{ Auth::user()->officialname }}</h5>
                     
                   <li class="mt">
                       <a class="active" href="index.html">
