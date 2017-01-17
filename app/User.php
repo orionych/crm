62f4acecd4879588aef30010b3e8d700afc74af3
= <=>
2017-01-17 16:03:23 +0300

    Auto-committing 'User.php'

--- app/User.php
+++ app/User.php
@@ -21,8 +21,7 @@ class User extends Authenticatable
         return $this->belongsToMany('App\Permission');
     }
     public function getNameAttribute($name) {
-        $this->officialname='Johhny';
-        return $name;
+          return $name;
     }
     public function getPermission()
     {
