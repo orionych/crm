710a062c16c0a938281a1bbd0f61926c38c0358d
= <=>
2017-01-17 22:23:02 +0300

    Auto-committing 'FirmsSeeder.php'

---
+++ database/seeds/FirmsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FirmsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $firms=DB::connection('oldcrm')->table('crm_firms')->all();
+        
+    }
+}
