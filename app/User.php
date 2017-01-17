a4b6d30b24e80e3c25690bbfba7c19a4f2e380cb
= <=>
2017-01-17 16:12:01 +0300

    Auto-committing 'User.php'

--- app/User.php
+++ app/User.php
@@ -29,6 +29,6 @@ class User extends Authenticatable
     }
     public function getAvatar()
     {
-        echo "<div class='circle'>АВ</div>";
+        echo "<img src='http://placehold.it/40x40' class='img_circle'>";
     }
 }
