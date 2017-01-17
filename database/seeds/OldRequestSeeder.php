f642ab71212dd452cc1b49c11b3cf294ad13e8ee
= <=>
2017-01-18 00:07:51 +0300

    Auto-committing 'OldRequestSeeder.php'

--- database/seeds/OldRequestSeeder.php
+++ database/seeds/OldRequestSeeder.php
@@ -24,8 +24,8 @@ class OldRequestSeeder extends Seeder
     		'id'	=>$request->id,
     		'request_type_id'	=>$request->typeid,
     		'client_id'	=> $request->clientid,
-    		'created_at'	=> Carbon::createFromTimestamp($request->time),
-
+    		'created_at'	=> Carbon::createFromTimestamp(strtotime($request->time)+$request->calltimeshift),
+    		
     		]);
     	}   
     }
