02b1a0f65e4529ad7e05dd7012b507ea0bbc3b45
= <=>
2017-01-17 23:30:46 +0300

    Auto-committing 'OldClientsSeeder.php'

--- database/seeds/OldClientsSeeder.php
+++ database/seeds/OldClientsSeeder.php
@@ -23,8 +23,8 @@ class OldClientsSeeder extends Seeder
     		Client::create([
     			'id'=>$client->id,
     			'name'		=> iconv("KOI8-R","UTF-8",$client->fio),
-    			'name'		=> iconv("KOI8-R","UTF-8",$client->fio),
-
+    			'firm_id'	=> iconv("KOI8-R","UTF-8",$client->firmid),
+    			'fired'	=> 	intval($client->fired)
     			]);
     	}
     }
