66341b9cbee5661770fdc2f09e17e8e889eee292
= <=>
2017-01-17 22:31:05 +0300

    Auto-committing '2017_01_17_191952_create_firm_table.php'

--- database/migrations/2017_01_17_191952_create_firm_table.php
+++ database/migrations/2017_01_17_191952_create_firm_table.php
@@ -16,13 +16,15 @@ class CreateFirmTable extends Migration
         Schema::table('firms', function (Blueprint $table) {
             $table->string('name')->default();
             $table->string('fullname')->default();
+            $table->string('okved')->default();
             $table->integer('power')->default(0);            
             $table->integer('stars')->default(0);    
             $table->integer('fizlico')->default(0);
             $table->integer('city_id')->default(0);
-            $table->integer('holding_id')->default(0);
-            
-            $table->string('fullname')->default();
+            $table->integer('holding_id')->default(0);            
+            $table->string('inn')->default();        
+            $table->string('kpp')->default();
+            $table->string('ogrn')->default();
 
         });
     }
