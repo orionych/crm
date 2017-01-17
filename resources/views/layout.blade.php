b764f556c9258d7f2431a4469abebef223182ffa
= <=>
2017-01-17 12:58:54 +0300

    Auto-committing 'layout.blade.php'

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,45 +12,6 @@
         
     </head>
     <body >
-    <div id='app'>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif 
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-<input type='button' value='Check' onclick=send() />
-                </div>
-            </div>
-        </div>
-</div>
-        <script src="/js/app.js"></script>
-        <script type="text/javascript"> 
-        function send(){
-              Vue.http.get('/someUrl').then((response) => {
-                alert(response.data+' '+document.getElementById('csrf_token').getAttribute('content'));
-    // success callback
-  }, (response) => {alert('error');
-    // error callback
-  });}
-        </script>
 
     </body>
 </html>
