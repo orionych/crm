8fafb819e73a67e9755ff94142f54d1bbe108f71
= <=>
2017-01-17 21:48:39 +0300

    Auto-committing 'User.php'

--- app/User.php
+++ app/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable
     }
     public function getOfficalName()
     {
-            return 'here';
+            return preg_replace('/(\w+) (\w)\w+ (\w)\w+/iu', '$1 $2. $3.', $this->name);
     }
     public function getAvatar()
     {
