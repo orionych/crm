3d3192ec6610591e794d7fc3c522db0812917cb7
= <=>
2017-01-17 13:49:24 +0300

    Auto-committing 'app.js'

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,12 +16,12 @@ require('vue-resource');
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
- */
+ */ 
 new Vue({
     el: '#app',
     data: {enabled:true}
 });
-// Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#csrf_token').getAttribute('content');
+Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#csrf_token').getAttribute('content');
 Vue.component('firm', require('./components/Firm.vue'));
 
 
