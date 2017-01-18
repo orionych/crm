9acad3f07c7aba835c883a268d86b7594c77c6b5
= <=>
2017-01-18 15:07:30 +0300

    Auto-committing '2017_01_18_120427_create_tnveds_table.php'

--- database/migrations/2017_01_18_120427_create_tnveds_table.php
+++ database/migrations/2017_01_18_120427_create_tnveds_table.php
@@ -15,7 +15,8 @@ class CreateTnvedsTable extends Migration
     {
         Schema::create('tnveds', function (Blueprint $table) {
             $table->primary('id');
-            $table->
+            $table->string('name');
+            
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
