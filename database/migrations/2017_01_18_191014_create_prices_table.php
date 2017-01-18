ea5db5c803881c8bc4785f89b8534c2e35b4c20d
= <=>
2017-01-18 22:11:25 +0300

    Auto-committing '2017_01_18_191014_create_prices_table.php'

---
+++ database/migrations/2017_01_18_191014_create_prices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id');
+            $table->double('price',15,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
