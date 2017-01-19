d80051bc584ce91de1246e4358c50715bb5a67f4
= <=>
2017-01-19 21:32:06 +0300

    Auto-committing '2017_01_19_180514_create_sectors_table.php'

--- database/migrations/2017_01_19_180514_create_sectors_table.php
+++ database/migrations/2017_01_19_180514_create_sectors_table.php
@@ -16,6 +16,7 @@ class CreateSectorsTable extends Migration
         Schema::create('sectors', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->default();
+            $table->boolean('enabled')->default(false);
         });
     }
 
