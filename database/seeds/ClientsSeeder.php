0de888ce3cfdc59022196539803e244e7d8c647c
= <=>
2017-01-19 22:16:50 +0300

    Auto-committing 'ClientsSeeder.php'

---
+++ database/seeds/ClientsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Client;
+use App\OldClient;
+use Illuminate\Database\Seeder;
+
+class ClientsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        	$clients=DB::connection('oldcrm')->table('crm_clients')->get();
+	DB::statement("SET FOREIGN_KEY_CHECKS=0");
+	DB::table('clients')->truncate();
+   	DB::statement("SET FOREIGN_KEY_CHECKS=1");
+    	Eloquent::unguard();    
+    	foreach ($clients as $client)
+    	{
+    		Client::create([
+    			'id'=>$client->id,
+    			'name'		=> iconv("KOI8-R","UTF-8",$client->fio),
+    			'firm_id'	=> iconv("KOI8-R","UTF-8",$client->firmid),
+    			'fired'	=> 	intval($client->fired)
+    			]);
+    	}
+    }
+}
