7def272f5cdfccb57f1612abcb5ce53715a417de
= <=>
2017-01-20 10:14:33 +0300

    Auto-committing '2017_01_20_070042_create_specifications_table.php'

--- database/migrations/2017_01_20_070042_create_specifications_table.php
+++ database/migrations/2017_01_20_070042_create_specifications_table.php
@@ -20,6 +20,9 @@ class CreateSpecificationsTable extends Migration
             $table->integer('number')->default(0);
             $table->text('text');
            // $table->integer('payment_id')->index()->default(0);
+            $table->string('payment')->default();
+            $table->integer('delivery')->default(0);
+            $table->integer('shift')->default(0);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
