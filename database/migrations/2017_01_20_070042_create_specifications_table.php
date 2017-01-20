f4b12f4c51490c43f1bfbdca2ad03b37ddb91286
= <=>
2017-01-20 10:20:14 +0300

    Auto-committing '2017_01_20_070042_create_specifications_table.php'

--- database/migrations/2017_01_20_070042_create_specifications_table.php
+++ database/migrations/2017_01_20_070042_create_specifications_table.php
@@ -24,6 +24,7 @@ class CreateSpecificationsTable extends Migration
             $table->integer('week')->default(0);
             $table->integer('shift')->default(0);
             $table->boolean('payer')->default(false);
+            
 
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
