7b9ed8c9580bb6354afef3d973dbd07570f22ba3
= <=>
2017-01-22 13:39:24 +0300

    Auto-committing '2017_01_22_103602_create_equipment_name_types_table.php'

--- database/migrations/2017_01_22_103602_create_equipment_name_types_table.php
+++ database/migrations/2017_01_22_103602_create_equipment_name_types_table.php
@@ -19,7 +19,7 @@ class CreateEquipmentNameTypesTable extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
-        DB::table('equipment_name_types')->insert(array('name'=>'Код оборудования'));
+        DB::table('equipment_name_types')->insert(array('name'=>'Код оборудования поставщика'));
         DB::table('equipment_name_types')->insert(array('name'=>'Код в документах'));
         DB::table('equipment_name_types')->insert(array('name'=>'Обезличенный код'));
         DB::table('equipment_name_types')->insert(array('name'=>'Id по бухгалтерии'));
