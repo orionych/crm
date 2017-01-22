229c601c6dc35bfa4a55a8138f690b9bcc369dd7
= <=>
2017-01-22 15:01:00 +0300

    Auto-committing '2017_01_18_191504_create_price_types_table.php'

--- database/migrations/2017_01_18_191504_create_price_types_table.php
+++ database/migrations/2017_01_18_191504_create_price_types_table.php
@@ -21,7 +21,11 @@ class CreatePriceTypesTable extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
-
+        // DB::table('price_types')->insert(array('name'=>'Закупка для нас','html'=>'!'));
+        // DB::table('price_types')->insert(array('name'=>'Закупка у производителя','html'=>'произв.'));
+        // DB::table('price_types')->insert(array('name'=>'Цена для ОЕМ компаний','html'=>'OEM'));
+        // DB::table('price_types')->insert(array('name'=>'Цена для EU','html'=>'$'));
+        // DB::table('price_types')->insert(array('name'=>'Себестоимость по бухгалтерии','html'=>'бух.','readonly'=>false));
     }
 
     /**
