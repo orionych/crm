5974fa577f1b280fe61b5763b64999d2bd2fcedf
= <=>
2017-01-22 13:07:01 +0300

    Auto-committing '2017_01_22_095751_create_dimensions_table.php'

--- database/migrations/2017_01_22_095751_create_dimensions_table.php
+++ database/migrations/2017_01_22_095751_create_dimensions_table.php
@@ -19,7 +19,7 @@ class CreateDimensionsTable extends Migration
             $table->double('dimx',8,3)->default(0);
             $table->double('dimy',8,3)->default(0);
             $table->double('dimh',8,3)->default(0);
-            $table->double('weight',8,3)->default(0);
+            $table->double('weight',9,3)->default(0);
             $table->double('places',8,5)->default(0);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
