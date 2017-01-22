032d0ef9e8a972573b615eebca2848278a47e1db
= <=>
2017-01-22 22:09:34 +0300

    Auto-committing 'OkvedSeeder.php'

---
+++ database/seeds/OkvedSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Okved;
+use Illuminate\Database\Seeder;
+
+class OkvedSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $contents=trim(File::get(base_path().'/database/seeds/okved2.csv'));
+        $arrays=explode("\n",$contents);
+        foreach ($arrays as $array)
+	{
+        	$sector=explode(',',$array);
+            $okveds=explode('.',$sector[0]);
+        Okved::create([
+        	'sector'=>intval($okveds[0]),
+            'subsector'=>intval($okveds[1]),
+            'suffix'=>$okveds[2],
+        	'name'=>str_replace('"','',$sector[1])
+        	]);
+    	}
+    }
+}
