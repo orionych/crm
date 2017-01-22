fda97a61c9293e83285d189679979f12d0f032b6
= <=>
2017-01-22 13:33:17 +0300

    Auto-committing '2017_01_22_103008_create_equipment_names_table.php'

--- database/migrations/2017_01_22_103008_create_equipment_names_table.php
+++ database/migrations/2017_01_22_103008_create_equipment_names_table.php
@@ -19,7 +19,7 @@ class CreateEquipmentNamesTable extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
-        DB::table('equipment_names')->insert(array('name'=>'Название наше'));
+        DB::table('equipment_names')->insert(array('name'=>'Наше название наше'));
         DB::table('equipment_names')->insert(array('name'=>'Название наше'));
     }
 
