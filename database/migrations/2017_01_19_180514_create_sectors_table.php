d16f5a7017d7158ae02126da077e58b5eeabe252
= <=>
2017-01-22 21:54:14 +0300

    Auto-committing '2017_01_19_180514_create_sectors_table.php'

--- database/migrations/2017_01_19_180514_create_sectors_table.php
+++ database/migrations/2017_01_19_180514_create_sectors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSectorsTable extends Migration
+class CreateOkvedsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSectorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sectors', function (Blueprint $table) {
+        Schema::create('okveds', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->default();
             $table->boolean('enabled')->default(false);
