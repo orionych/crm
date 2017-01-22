c3cdf251ec8abb8823b37c20c389e38d4cd95957
= <=>
2017-01-22 13:03:58 +0300

    Auto-committing '2017_01_22_095751_create_dimensions_table.php'

--- database/migrations/2017_01_22_095751_create_dimensions_table.php
+++ database/migrations/2017_01_22_095751_create_dimensions_table.php
@@ -15,7 +15,7 @@ class CreateDimensionsTable extends Migration
     {
         Schema::create('dimensions', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('equipment_id');
+            $table->integer('equipment_id')->index();
             $table->double('dimx',6,3)->default(0);
             $table->double('dimy',6,3)->default(0);
             $table->double('dimh',6,3)->default(0);
