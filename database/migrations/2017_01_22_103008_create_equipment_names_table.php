50fdabc8258ede78ba785e0b58095c7666f93efa
= <=>
2017-01-22 13:31:47 +0300

    Auto-committing '2017_01_22_103008_create_equipment_names_table.php'

--- database/migrations/2017_01_22_103008_create_equipment_names_table.php
+++ database/migrations/2017_01_22_103008_create_equipment_names_table.php
@@ -21,7 +21,7 @@ class CreateEquipmentNamesTable extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
-        DB::table('equipment_names')->insert('equi');
+        DB::table('equipment_names')->insert('equip');
     }
 
     /**
