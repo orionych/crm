41f9661962f1ebd75fab2d59edd08e62c623af35
= <=>
2017-01-22 16:18:53 +0300

    Auto-committing '2017_01_22_131712_create_offer_versions_table.php'

---
+++ database/migrations/2017_01_22_131712_create_offer_versions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOfferVersionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offer_versions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('offer_id')->index()->default(0);
+            $table->integer('version')->index()->default(0);
+            
+
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offer_versions');
+    }
+}
