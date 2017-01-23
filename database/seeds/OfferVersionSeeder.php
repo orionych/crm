d9530c1bf57c150ab663385546957a0b4b89cf54
= <=>
2017-01-23 21:15:54 +0300

    Auto-committing 'OfferVersionSeeder.php'

---
+++ database/seeds/OfferVersionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OfferVersionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	$offers=DB::connection('oldcrm')->table('crm_clients_commercial_offer')->get();
+	DB::statement("SET FOREIGN_KEY_CHECKS=0");
+	DB::table('offers')->truncate();
+   	DB::statement("SET FOREIGN_KEY_CHECKS=1");   	
+    	Eloquent::unguard(); 
+    }
+}
