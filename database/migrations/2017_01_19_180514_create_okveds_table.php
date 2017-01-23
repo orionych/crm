b98c54291c8d506fe7c6253928a28c8982455bda
= <=>
2017-01-23 19:26:06 +0300

    Auto-committing '2017_01_19_180514_create_okveds_table.php'

--- database/migrations/2017_01_19_180514_create_okveds_table.php
+++ database/migrations/2017_01_19_180514_create_okveds_table.php
@@ -20,7 +20,7 @@ class CreateOkvedsTable extends Migration
             $table->integer('suffix')->default(0);
             $table->string('name')->default();
             $table->boolean('enabled')->default(false);
-            $table->u(['sector','subsector','suffix']);
+            $table->uu(['sector','subsector','suffix']);
         });
     }
 
