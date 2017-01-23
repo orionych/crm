f6d57fb444abb40690514d150b07d736d2b7e4f7
= <=>
2017-01-23 20:17:49 +0300

    Auto-committing 'Firm.php'

--- app/Firm.php
+++ app/Firm.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Firm extends Model
 {
-    public function okveds() {
-    	return $this->belongsTo('App\Okved');
-    }
+	public function okveds() {
+	   	return $this->belongsTo('App\Okved');
+	}
+    	public function clients()
+	{
+	        return $this->hasMany('App\Client');
+	}
 }
