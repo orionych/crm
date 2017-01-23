696054ece5984055d57e5610fd9bcb72ee2f1cd3
= <=>
2017-01-23 21:33:17 +0300

    Auto-committing 'Okv.php'

--- app/Okv.php
+++ app/Okv.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Okv extends Model
 {
-    public function oldId($id) {
+    public static function oldId($id) {
     	$okvs=array(0,643,978);    	
     	return $okvs[$id];
     }
