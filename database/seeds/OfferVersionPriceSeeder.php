6c343683761be1b44fc25bbeaecaf1f29740abc4
= <=>
2017-01-23 22:25:44 +0300

    Auto-committing 'OfferVersionPriceSeeder.php'

--- database/seeds/OfferVersionPriceSeeder.php
+++ database/seeds/OfferVersionPriceSeeder.php
@@ -19,7 +19,7 @@ class OfferVersionPriceSeeder extends Seeder
     	Eloquent::unguard(); 
     	foreach ($offers as $offer)
     	{
-    		$o=OfferVersion::where('offer_id',$offer->id)->first();
+    		$o=OfferVersion::where('offer_id',$offer->id)->get();
     		print_r($o);
     		//$o->prices()->attach([$offer->priceid,['count'=>$offer->countitem,'price'=>$offer->price,'time'=>$offer->individualtime]]);
     	}
