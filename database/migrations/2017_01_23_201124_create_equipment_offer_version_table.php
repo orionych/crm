3d6df7a3065188168e07da9960e4e89b02f02cf6
= <=>
2017-01-23 23:12:57 +0300

    Auto-committing '2017_01_23_201124_create_equipment_offer_version_table.php'

---
+++ database/migrations/2017_01_23_201124_create_equipment_offer_version_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEquipmentOfferVersionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment_offer_version', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('equipment_id');
+            $table->integer('offer_version_id')->index();
+            $table->double('count',14,4)->default(0);
+            $table->
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment_offer_version');
+    }
+}
