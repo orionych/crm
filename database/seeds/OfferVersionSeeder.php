c3b90ed3d19727f58750a52daeed307bd4878744
= <=>
2017-01-23 21:26:26 +0300

    Auto-committing 'OfferVersionSeeder.php'

--- database/seeds/OfferVersionSeeder.php
+++ database/seeds/OfferVersionSeeder.php
@@ -30,7 +30,8 @@ class OfferVersionSeeder extends Seeder
     			'offer_id'	=>$offer->id,
     			'version'	=>1,
     			'name'		=>iconv('KOI8-R','UTF-8',$offer->name),
-    			'okv_id'	=>Okv::
+    			'okv_id'	=>Okv::oldId($offer->currency),
+    			
     			]);
     	}
     }
