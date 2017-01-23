e12f094a7f48f7efe050a9f32ebb9a146367fd0e
= <=>
2017-01-23 19:46:28 +0300

    Auto-committing 'FirmsSeeder.php'

--- database/seeds/FirmsSeeder.php
+++ database/seeds/FirmsSeeder.php
@@ -23,7 +23,7 @@ class FirmsSeeder extends Seeder
     			'id'		=>$firm->id,
     			'name'		=> iconv("KOI8-R","UTF-8",$firm->name),
     			'fullname'	=> iconv("KOI8-R","UTF-8",$firm->fullname),
-    			'okved_id'	=> substr(iconv("KOI8-R","UTF-8",$firm->okved),0,10),
+    			'okved_id'	=> substr(iconv("KOI8-R","UTF-8",$firm->okved),0,9),
     			'power'		=> iconv("KOI8-R","UTF-8",$firm->power),
     			'stars'		=> iconv("KOI8-R","UTF-8",$firm->stars),
     			'fizlico'		=> iconv("KOI8-R","UTF-8",$firm->fizlico),
