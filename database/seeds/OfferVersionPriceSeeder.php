691b150ebf317393149f141f8121687ed3010d57
= <=>
2017-01-23 23:19:03 +0300

    Auto-committing 'OfferVersionPriceSeeder.php'

--- database/seeds/OfferVersionPriceSeeder.php
+++ database/seeds/OfferVersionPriceSeeder.php
@@ -21,7 +21,7 @@ class OfferVersionPriceSeeder extends Seeder
     	{
     		$o=OfferVersion::where('offer_id',$offer->offerid)->first();
     		
-    		if (is_object($o)) $o->prices()->attach($offer->priceid,['count'=>$offer->countitem,'price'=>$offer->price,'time'=>$offer->individualtime]);
+    		if (is_object($o)) $o->equipments()->attach($offer->priceid,['count'=>$offer->countitem,'price'=>$offer->price,'time'=>$offer->individualtime]);
 
     	}
     }
