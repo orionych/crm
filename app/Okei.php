a63bef0c24ec45a7d25356fe703c3f152045148c
= <=>
2017-01-24 17:47:24 +0300

    Auto-committing 'Okei.php'

--- app/Okei.php
+++ app/Okei.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Okei extends Model
 {
-    //
+    public static function oldArray($id) {
+    	$okei=array(0,796,18,839);
+    	return $okey[$id];
+    }
 }
