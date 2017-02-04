444ad622e42b239c3795678391fc05e2827b8827
= <=>
2017-02-04 20:10:02 +0300

    Auto-committing 'My-input.vue'

--- resources/assets/js/components/My-input.vue
+++ resources/assets/js/components/My-input.vue
@@ -10,7 +10,7 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            alert(placeholder);
         },
         props: ['placeholder']
     }
