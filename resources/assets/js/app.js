1886574844e49bab845adb625a680d42bfbcb86f
= <=>
2017-02-04 17:10:11 +0300

    Auto-committing 'app.js'

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,10 +17,32 @@ require('vue-resource');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */ 
-new Vue({
- 	el: '#app',
- 	data: {show:true}
- });
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+// 2. Определение путей
+// Каждый путь должен указывать на компонент
+// "Компонентом" может быть как созданный через Vue.extend()
+// полноценный конструктор, так и просто объект с настройками компонента
+// Вложенные пути будут рассмотрены далее.
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+// 3. Создаём инстанс роутера с опцией `routes`
+// Можно передать и другие опции, но пока не будем усложнять
+const router = new VueRouter({
+  routes // сокращение от routes: routes
+})
+
+// 4. Создаём и монтируем корневой инстанс Vue нашего приложения.
+// Удостоверьтесь, что передали инстанс роутера в опции `router`,
+// что позволит приложению знать о его наличии
+const app = new Vue({
+  router
+}).$mount('#app')
+
 
 Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#csrf_token').getAttribute('content');
 Vue.component('firm', require('./components/Firm.vue'));
