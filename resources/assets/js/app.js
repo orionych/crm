01b7e466d906a5f6e707d16457f9eb8e6ef3269f
= <=>
2017-02-04 14:22:59 +0300

    Auto-committing 'app.js'

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,7 +17,7 @@ require('vue-resource');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */ 
-new Vue({
+var dashboard=new Vue({
     el: '#dashboard',
     data: {enabled:true}
 });
