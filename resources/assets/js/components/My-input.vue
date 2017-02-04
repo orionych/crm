0a1df94983dfd2d0d7cd87b6156b4d52d4cf0fa4
= <=>
2017-02-04 21:03:33 +0300

    Auto-committing 'My-input.vue'

--- resources/assets/js/components/My-input.vue
+++ resources/assets/js/components/My-input.vue
@@ -37,4 +37,10 @@
 	top:20px;
 	left:0px;
 	}
+	.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0
+}
 </style>
