bf1dbc21c6402949cb0f2451a19378a6de761419
= <=>
2017-02-04 19:49:02 +0300

    Auto-committing 'app.js'

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -20,7 +20,7 @@ Vue.use(VueRouter);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */ 
  Vue.component('firm',require('./components/Firm.vue'));
- Basket=Vue.component('basket',require('./components/Basket.vue'));
+Vue.component('basket',require('./components/Basket.vue'));
 
 
 const Foo = { template: '<div>foo<basket>1</basket></div>' }
