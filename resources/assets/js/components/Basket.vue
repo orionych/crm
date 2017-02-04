f16e26fa49270562e611f79441e08e9a4b19d1b8
= <=>
2017-02-04 20:38:46 +0300

    Auto-committing 'Basket.vue'

--- resources/assets/js/components/Basket.vue
+++ resources/assets/js/components/Basket.vue
@@ -7,7 +7,7 @@
                     <div class="panel-body">
                          <div class="row">
                             <div class='col-lg-6'>
-                            <my-input placeholder='Название2' name='name'></my-input>
+                            <my-input placeholder='Название2' name='nameof'></my-input>
                             </div>
                          </div>
                     </div>
