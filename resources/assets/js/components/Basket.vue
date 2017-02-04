605e87bfbb5fd3ea739303ba6bef7daa0c6d8c21
= <=>
2017-02-04 19:58:15 +0300

    Auto-committing 'Basket.vue'

--- resources/assets/js/components/Basket.vue
+++ resources/assets/js/components/Basket.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container" style='width:100%'>
         <div class="row">
             <div class="col-lg-6">
                 <div class="panel panel-default">
