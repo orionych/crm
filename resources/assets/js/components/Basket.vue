54d21dc73fae65be55612d3a149e924901a0813c
= <=>
2017-02-04 20:01:48 +0300

    Auto-committing 'Basket.vue'

--- resources/assets/js/components/Basket.vue
+++ resources/assets/js/components/Basket.vue
@@ -7,7 +7,7 @@
                     <div class="panel-body">
                          <div class="row">
                             <div class='col-lg-6'>
-                            <input type='text' value='Название' name='name' />
+                            <input type='text' p='Название' name='name' />
                             </div>
                          </div>
                     </div>
