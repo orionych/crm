e4a7ddde27244752c199c7c67482eee3cc4d1169
= <=>
2017-02-04 20:09:13 +0300

    Auto-committing 'My-input.vue'

--- resources/assets/js/components/My-input.vue
+++ resources/assets/js/components/My-input.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
     	<label>
-    		<input type='text' name='name' value='' placeholder='{{ placeholder }}' />
+    		<input type='text' name='name' value='' :placeholder='placeholder' />
     	</label>
     </div>
 </template>
