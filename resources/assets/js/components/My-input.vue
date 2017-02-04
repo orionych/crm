13c48307587cc972b4688f60fb6906421e4d8c10
= <=>
2017-02-04 21:48:29 +0300

    Auto-committing 'My-input.vue'

--- resources/assets/js/components/My-input.vue
+++ resources/assets/js/components/My-input.vue
@@ -54,5 +54,26 @@
 	.fade-enter-active {
 	 animation: bounce-in .5s;
 	}
-
+	@keyframes bounce-in {
+	  0% {
+	    transform: scale(0);
+	  }
+	  50% {
+	    transform: scale(1.5);
+	  }
+	  100% {
+	    transform: scale(1);
+	  }
+	}
+	@keyframes bounce-out {
+	  0% {
+	    transform: scale(1);
+	  }
+	  50% {
+	    transform: scale(1.5);
+	  }
+	  100% {
+	    transform: scale(0);
+	  }
+	}
 </style>
