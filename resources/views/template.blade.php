0ff24b1e0bd95b3869996f7ead44e3186dde7927
= <=>
2017-02-04 13:49:42 +0300

    Auto-committing 'template.blade.php'

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,24 +1,20 @@
  <!DOCTYPE html>
-<html lang="ru">
-    <head>
-	<meta charset="utf-8">
-	<meta name="csrf-token" id="csrf_token" content="{{ csrf_token() }}" >
+ <html lang="ru">
+ <head>
+   <meta charset="utf-8">
+   <meta name="csrf-token" id="csrf_token" content="{{ csrf_token() }}" >
 
-        <title>CRM система. ООО "Лидер"</title>
-   
-        <!-- Styles -->
-        <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css">
-        <link href="/css/app.css" rel="stylesheet" type="text/css">
-        <link href="/css/dashio.css" rel="stylesheet" type="text/css">
-        <link href="/css/dashio-responsive.css" rel="stylesheet">
-        <link href="/css/font-awesome.css" rel="stylesheet">
-  	<div>
-       hello world 
-    </div>
-    <div>
-        tab
-    </div>
-    </head>
+   <title>CRM система. ООО "Лидер"</title>
+
+   <!-- Styles -->
+   <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css">
+   <link href="/css/app.css" rel="stylesheet" type="text/css">
+   <link href="/css/dashio.css" rel="stylesheet" type="text/css">
+   <link href="/css/dashio-responsive.css" rel="stylesheet">
+   <link href="/css/font-awesome.css" rel="stylesheet">
+    <script type='text/javascript' src='/js/app.js'></script>
+</head>
     <body>
+        
     </body>
    </html>
