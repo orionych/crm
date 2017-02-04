7650c660892398c37ff67b67b14e5c7294495a29
= <=>
2017-02-04 14:23:51 +0300

    Auto-committing 'app.js'

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,10 +17,15 @@ require('vue-resource');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */ 
-var dashboard=new Vue({
-    el: '#dashboard',
-    data: {enabled:true}
-});
+ var dashboard=new Vue({
+ 	el: '#dashboard',
+ 	data: {enabled:true}
+ });
+ var basket=new Vue(
+ {
+ 	el: "#basket",
+ 	data: {vat:18,show:false} 
+ })
 Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#csrf_token').getAttribute('content');
 Vue.component('firm', require('./components/Firm.vue'));
 
