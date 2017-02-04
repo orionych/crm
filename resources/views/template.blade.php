44c34d24a6fc428294a69d509283bf6f1bd9b106
= <=>
2017-02-04 13:43:31 +0300

    Auto-committing 'template.blade.php'

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,1 +1,17 @@
- 
+ <!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta name="csrf-token" id="csrf_token" content="{{ csrf_token() }}" >
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title>CRM система. ООО "Лидер"</title>
+   
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+        <link href="/css/dashio.css" rel="stylesheet" type="text/css">
+        <link href="/css/dashio-responsive.css" rel="stylesheet">
+        <link href="/css/font-awesome.css" rel="stylesheet">
+  
+    </head>
