7095a72c960061a028379d17a30ccd01e97d1c3f
= <=>
2017-02-06 14:50:24 +0300

    Auto-committing 'My-input.vue'

--- resources/assets/js/components/My-input.vue
+++ resources/assets/js/components/My-input.vue
@@ -13,7 +13,7 @@
         mounted() {
             console.log('input mounted');
         },
-        props: ['placeholder','name','value'],
+        props: ['placeholder','value'],
         methods: {
 	    toggle: function () {
 	      this.$emit('input',  { target: { value: this.value } });
