4196e2c62b2e4a8df99f56694a4120223abe8740
= <=>
2017-02-06 15:25:42 +0300

    Auto-committing 'Basket.vue'

--- resources/assets/js/components/Basket.vue
+++ resources/assets/js/components/Basket.vue
@@ -8,7 +8,7 @@
                          <div class="row">
                             <div class='col-lg-6'>
                             <my-input placeholder='Название коммерческого предложения' v-model='name' value='2'  ></my-input>
-                            <my-input placeholder='Компания' name='company' value='1' ></my-input>
+                            <my-input placeholder='Компания' value='1' ></my-input>
                             </div>
                              <div class='col-lg-6'>
                              <span>{{ name }}</span>
