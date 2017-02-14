7de5f35c20378d5a25105862c97fb67742515f6a
= <=>
2017-02-14 19:48:03 +0300

    Auto-committing 'UserOnlinesController.php'

--- app/Http/Controllers/UserOnlinesController.php
+++ app/Http/Controllers/UserOnlinesController.php
@@ -13,7 +13,7 @@ public function __construct()
 	}
 public function start(Request $request)
 	{	
-		d($request);
+		dd($request);
 		$useronline=new UserOnline;
 		$useronline->user_id=$this->auth_user->id;
 		$useronline->save();
