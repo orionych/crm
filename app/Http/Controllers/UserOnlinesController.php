cce7c37623f44afef0ce03d976c0ea9289e0c45b
= <=>
2017-02-14 22:08:12 +0300

    Auto-committing 'UserOnlinesController.php'

--- app/Http/Controllers/UserOnlinesController.php
+++ app/Http/Controllers/UserOnlinesController.php
@@ -15,7 +15,7 @@ public function __construct()
 	}
 public function start(Request $request)
 	{	
-		$useronline=UserOnline::where('started_at','>',Carbon::now()->format('Y-m-d'))->select(DB::raw(' SUM(TIMEDIFF(ended_at,started_at)) as time'))->get('');
+		$useronline=UserOnline::where('started_at','>',Carbon::now()->format('Y-m-d'))->select(DB::raw(' SUM(TIMEDIFF(ended_at,started_at)) as time'))->get();
 		dd($useronline->toArray());
 		unset($useronline);
 		$useronline=new UserOnline;
