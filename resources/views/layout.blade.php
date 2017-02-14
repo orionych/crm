9548324e940a06549859441be691368eaafa88f8
= <=>
2017-02-15 01:28:22 +0300

    Auto-committing 'layout.blade.php'

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -110,7 +110,7 @@
                     <!-- clock -->
                     <li>               
                       <router-link to="/clock"> 
-                              <clock :></clock>         
+                              <clock value="21:14:21"></clock>         
                       </router-link> 
                     </li>
                     <!-- clock end -->
