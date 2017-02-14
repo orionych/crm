deeb28aa0757b4070945ff0b5cb93056ea928f49
= <=>
2017-02-14 19:21:19 +0300

    Auto-committing 'template.blade.php'

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -35,7 +35,7 @@
       window.onload=function() {
         window.onfocus=function(){
         console.log('started');
-        Vue.http.get('/user_online/').then(response => {
+        Vue.http.get('/user_online/c').then(response => {
               useronlineid=response.body.id;
             }, response => {
               alert('error');
