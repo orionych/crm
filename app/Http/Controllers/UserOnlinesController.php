becf8c57d4423e820214c911653e21836079f6b9
= <=>
2017-02-14 21:53:22 +0300

    Auto-committing 'UserOnlinesController.php'

--- app/Http/Controllers/UserOnlinesController.php
+++ app/Http/Controllers/UserOnlinesController.php
@@ -15,7 +15,7 @@ public function __construct()
 	}
 public function start(Request $request)
 	{	
-		$maxtime=UserOnline::where('started_at','>',Carbon::now()->format('Y-m-d'))->select(DB::raw(' TIMEDIFF(ended_at,started_at) as time'))->get();
+		$maxtime=UserOnline::where('started_at','>',Carbon::now()->format('Y-m-d'))->select(DB::raw(' TIMEDIFF(ended_at,started_at) as time'))->sum('time');
 		dd($maxtime);
 		unset($useronline);
 		$useronline=new UserOnline;
