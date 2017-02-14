eec464b2b536ddbb465d256cb881713e34bd9c6c
= <=>
2017-02-14 22:19:06 +0300

    Auto-committing 'UserOnlinesController.php'

--- app/Http/Controllers/UserOnlinesController.php
+++ app/Http/Controllers/UserOnlinesController.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use App\UserOnline;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Auth\Access\Response;
 
 class UserOnlinesController extends Controller
 {
