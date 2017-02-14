01f5961d3176ad97d5b4c12d947fc04cf3f778f7
= <=>
2017-02-15 01:23:43 +0300

    Auto-committing 'layout.blade.php'

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -110,7 +110,7 @@
                     <!-- clock -->
                     <li>               
                       <router-link to="/clock"> 
-                              <i class="fa fa-clock-o"> 24:</i><clock></clock>         
+                              <i class="fa fa-clock-o"> 24:1</i><clock></clock>         
                       </router-link> 
                     </li>
                     <!-- clock end -->
