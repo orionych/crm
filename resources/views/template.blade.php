0b5f2f7ddc039997f9f1367e803007ac488f5741
= <=>
2017-02-14 19:10:52 +0300

    Auto-committing 'template.blade.php'

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -32,7 +32,7 @@
     <script type='text/javascript' src='/js/app.js'></script>
     <script type="text/javascript">
       window.onfocus=function(){
-        this.$http.get('/user_online',{id:100}).then(function(reponse){console.log(response);});
+        this.$http.get('/user_online',{id:100}).then(function(reponse){console.log(response);alert('here');});
       }
       window.onblur=function(){
         console.log('1 closed');
