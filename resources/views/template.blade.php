1bc3c53beea58e786e051de499537ff9208da3c9
= <=>
2017-02-14 20:25:50 +0300

    Auto-committing 'template.blade.php'

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -56,7 +56,7 @@
         alert('closed');
       }
       </script>
-{{ date_default_timezone_set('Europe/Moscow') }}
+{{ date_default_timezone_set() }}
       {{ date('Y-m-d H:i:s') }}
     </body>
 </html>
