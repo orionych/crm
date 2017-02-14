6e18b3652affa6a7d674ce3801b50b861f6a3854
= <=>
2017-02-15 00:59:57 +0300

    Auto-committing 'template.blade.php'

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -31,36 +31,7 @@
     
     <script type='text/javascript' src='/js/app.js'></script>
     <script type="text/javascript">
-      var useronlines_id=0;
-      var useronlinestarted=false;
-      function userOnlineRegister(){
-        if (!useronlinestarted) Vue.http.get('/user_online/start').then(response => {
-              useronlinestarted=true;
-              useronlines_id=response.body.online.id;
-              console.log(response.body.status);
-            }, response => {
-              alert('error');
-            });
-      }
-      window.onload=userOnlineRegister;
-      window.onfocus=userOnlineRegister;
 
-      window.onblur=function(){
-        Vue.http.post('/user_online/end',{id: useronlines_id}).then(response => {
-              useronlinestarted=false;
-              console.log(response.body);
-            }, response => {
-              alert('error');
-            });
-      }
-      window.onbeforeunload=function(){
-            if (useronlinestarted) Vue.http.post('/user_online/end',{id: useronlines_id}).then(response => {
-              useronlinestarted=false;
-              console.log(response.body);
-            }, response => {
-              alert('error');
-            });
-      }
       </script>
     </body>
 </html>
