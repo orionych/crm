8adb84c7610d5d506d64b8eaaf517bba6a9b59de
= <=>
2017-02-15 02:04:41 +0300

    Auto-committing 'app.js'

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -62,7 +62,6 @@ Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#csrf_token').
         if (!useronlinestarted) Vue.http.get('/user_online/start').then(response => {
               useronlinestarted=true;
               useronlines_id=response.body.online.id;
-              alert(clock.value);
               console.log(response.body.status);
             }, response => {
               console.log('error');
