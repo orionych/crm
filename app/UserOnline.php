2d8638759ac77134340c636376382eadbc227258
= <=>
2017-02-15 01:36:48 +0300

    Auto-committing 'UserOnline.php'

--- app/UserOnline.php
+++ app/UserOnline.php
@@ -9,10 +9,6 @@ class UserOnline extends Model
 	public $timestamps = false;
 	public $user_id;
 	protected $fillable = ['user_id','ended_at'];
-	function __construct()
-	{
-		$this->user=\Auth::user();
-	}
 	public function getTotalTime() {
 		return 10;
 	}
