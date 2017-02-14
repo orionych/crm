e6cc5c2674be78c40dca609e6139a4b448c87d46
= <=>
2017-02-15 01:43:31 +0300

    Auto-committing 'UserOnline.php'

--- app/UserOnline.php
+++ app/UserOnline.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class UserOnline extends Model
 {
 	public $timestamps = false;
+	private $userid;
 	protected $fillable = ['user_id','ended_at'];
+	function __construct($userid)
+	{
+		$this->user_id=$userid;
+	}
 	public function getTotalTime() {
 		return 10;
 	}
