f17cc63bcc8d4224f2fbbbb79664733b2d39b714
= <=>
2017-02-26 23:30:12 +0300

    Auto-committing 'FirmDetailsSeeder.php'

--- database/seeds/FirmDetailsSeeder.php
+++ database/seeds/FirmDetailsSeeder.php
@@ -16,6 +16,8 @@ class FirmDetailsSeeder extends Seeder
     {
 	DB::statement("SET FOREIGN_KEY_CHECKS=0");
   	DB::table('firm_details')->truncate();
+  	DB::table('country_firm_detail')->truncate();
+  	DB::table('firm_detail_firm_type')->truncate();
    	DB::statement("SET FOREIGN_KEY_CHECKS=1");
     	Eloquent::unguard(); 
   	$country=Country::find(643);
