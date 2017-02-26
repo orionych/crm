8f3f3deb2bffc54429f292d6bfcd4bcf07d87258
= <=>
2017-02-26 22:57:41 +0300

    Auto-committing 'Country.php'

--- app/Country.php
+++ app/Country.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    	protected $table = 'countries';
+    	// protected $table = 'countries';
 	public $timestamps = false;
 	public $incrementing = false;
 	public function firm_details() {
