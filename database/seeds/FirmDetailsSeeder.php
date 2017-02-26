e6b12caf44a3100b75ebf826f3a1d6b909e51a6c
= <=>
2017-02-26 22:25:05 +0300

    Auto-committing 'FirmDetailsSeeder.php'

--- database/seeds/FirmDetailsSeeder.php
+++ database/seeds/FirmDetailsSeeder.php
@@ -13,32 +13,26 @@ class FirmDetailsSeeder extends Seeder
     {
 	$o=FirmDetail::create([
     			'id'		=>1,
-    			'firm_type_id'	=>0,
     			'name'	=>'ИНН'
     			]);
 	$o=FirmDetail::create([
     			'id'		=>2,
-    			'firm_type_id'	=>1,
     			'name'	=>'КПП'
     			]);
 	$o=FirmDetail::create([
     			'id'		=>3,
-    			'firm_type_id'	=>1,
     			'name'	=>'ОГРН'
     			]);
 	$o=FirmDetail::create([
     			'id'		=>4,
-    			'firm_type_id'	=>1,
     			'name'	=>'Юридический адрес'
     			]);
 	$o=FirmDetail::create([
     			'id'		=>5,
-    			'firm_type_id'	=>1,
     			'name'	=>'Фактический адрес'
     			]);
 	$o=FirmDetail::create([
     			'id'		=>6,
-    			'firm_type_id'	=>1,
     			'name'	=>'Почтовый адрес'
     			]);
     }
