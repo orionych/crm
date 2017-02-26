88f3c916efe465f123fe7319cdf6ac0333e9c725
= <=>
2017-02-26 22:51:11 +0300

    Auto-committing '2017_02_26_224318_create_country_firm_detail_table.php'

---
+++ database/migrations/2017_02_26_224318_create_country_firm_detail_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountryFirmDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('country_firm_detail', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('country_id')->default(0);
+            $table->integer('firm_detail_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('country_firm_detail');
+    }
+}
