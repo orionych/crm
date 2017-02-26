132caa6561b3b2fa32152fcf9048d928dd0fa66a
= <=>
2017-02-26 13:25:52 +0300

    Auto-committing 'Price-list.vue'

--- resources/assets/js/components/Price-list.vue
+++ resources/assets/js/components/Price-list.vue
@@ -2,7 +2,7 @@
     <div class="container" style='width:100%'>
         <div class="row">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Корзина заказа</div>
+                    <div class="panel-heading">Прайс-лист: <input type='text' :v-model='code'</div>
                     <div class="panel-body">
                          <div class="row">
                             <div class='col-lg-6'>
