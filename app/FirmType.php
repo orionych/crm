04c7cba152323f2836d3aec002b41935f63d520a
= <=>
2017-02-26 23:26:12 +0300

    Auto-committing 'FirmType.php'

--- app/FirmType.php
+++ app/FirmType.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class FirmType extends Model
 {
 	public $timestamps = false;
+	public function firm_details() {
+		return $this->belongsToMany('App\FirmDetail');		
+	}
 }
