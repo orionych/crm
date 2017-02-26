84d34e41524995674f042cd7431b79a9ab79a969
= <=>
2017-02-26 13:25:05 +0300

    Auto-committing 'Price-list.vue'

--- resources/assets/js/components/Price-list.vue
+++ resources/assets/js/components/Price-list.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container col-lg-6" style='width:100%'>
+    <div class="container" style='width:100%'>
         <div class="row">
                 <div class="panel panel-default">
                     <div class="panel-heading">Корзина заказа</div>
