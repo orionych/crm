0ad5ff66ab6af4f99d05f4034b89cc43fbaca8d4
= <=>
2017-02-26 22:26:55 +0300

    Auto-committing '2017_02_26_222606_create_firm_banks_table.php'

---
+++ database/migrations/2017_02_26_222606_create_firm_banks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFirmBanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('firm_banks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('firm_id');
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('firm_banks');
+    }
+}
