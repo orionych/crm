aca0c1bdfdfc25c12ec023a6bb47647112f670dc
= <=>
2017-02-26 21:40:30 +0300

    Auto-committing '2017_01_17_191952_create_firm_table.php'

--- database/migrations/2017_01_17_191952_create_firm_table.php
+++ database/migrations/2017_01_17_191952_create_firm_table.php
@@ -22,9 +22,9 @@ class CreateFirmTable extends Migration
             $table->boolean('supplier')->default(false);
             $table->integer('power')->default(0);            
             $table->integer('stars')->default(0);    
+            $table->integer('city_id')->default(0);
             $table->string('okved_id',10)->index()->default();
             $table->integer('fizlico')->default(0);
-            $table->integer('city_id')->default(0);
             $table->integer('holding_id')->default(0);            
             $table->string('inn')->default();        
             $table->string('kpp')->default();
