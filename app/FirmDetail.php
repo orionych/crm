2e334bde01db82c32775209f4dd91a0bdc132077
= <=>
2017-02-26 22:45:59 +0300

    Auto-committing 'FirmDetail.php'

--- app/FirmDetail.php
+++ app/FirmDetail.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class FirmDetail extends Model
 {
 	public $timestamps = false;
-	
+	public function countries() {
+		return $this->belongsToMany('App\Countries');		
+	}
 
 }
