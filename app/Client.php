68d500f6c2c2e1aa073ff144e533c858a1faf1cb
= <=>
2017-03-01 10:46:26 +0300

    Auto-committing 'Client.php'

--- app/Client.php
+++ app/Client.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-	    public function firm()
+	public function firm()
 	    {
 	        return $this->belongsTo('App\Firm');
 	    }
+	public function requests()
+	{
+	        return $this->hasMany('App\Request');
+	}
 }
