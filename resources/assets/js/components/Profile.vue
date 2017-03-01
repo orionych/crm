52a3aa3228960accdc446561df6d1d285e8a94f5
= <=>
2017-03-01 13:55:07 +0300

    Auto-committing 'Profile.vue'

--- resources/assets/js/components/Profile.vue
+++ resources/assets/js/components/Profile.vue
@@ -1,11 +1,13 @@
 <template>
 	<div class='row content-panel'>
 		<div class='col-lg-4 profile-text mt mb centered'>
+		1
 		</div>
 		<div class='col-lg-4 profile-text mt mb centered'>
+		2
 		</div>
 		<div class='col-lg-4 profile-text mt mb centered'>
+		3
 		</div>
-		
 	</div>
 </template>
