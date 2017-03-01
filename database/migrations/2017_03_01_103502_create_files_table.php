9468090213f6d339f1ed17166b01dba4a69f910a
= <=>
2017-03-01 11:03:21 +0300

    Auto-committing '2017_03_01_103502_create_files_table.php'

--- database/migrations/2017_03_01_103502_create_files_table.php
+++ database/migrations/2017_03_01_103502_create_files_table.php
@@ -16,7 +16,9 @@ class CreateFilesTable extends Migration
         Schema::create('files', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->default();
-            $table->string('original_name')->default();            
+            $table->string('original_name')->default();   
+            $table->integer('fileable_id')->default(0);
+            $table->string('fileable_type')->default();         
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
