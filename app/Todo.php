ddb79d9adfd938d3241afe0448a080707b664a9c
= <=>
2017-03-01 09:50:59 +0300

    Auto-committing 'Todo.php'

--- app/Todo.php
+++ app/Todo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-public function users() {
-	$this->belongsTo('App\User');
-}
+	public function users() {
+		$this->belongsTo('App\User');
+	}
 }
