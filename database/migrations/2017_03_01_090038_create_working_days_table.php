95e386a6b5ea765d5ca37da9c27972cf8bd0d76f
= <=>
2017-03-01 09:33:29 +0300

    Auto-committing '2017_03_01_090038_create_working_days_table.php'

---
+++ database/migrations/2017_03_01_090038_create_working_days_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkingDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('working_days', function (Blueprint $table) {
+            $table->increments('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('working_days');
+    }
+}
--- database/seeds/FirmTypesSeeder.php
+++ database/seeds/FirmTypesSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\FirmType;
+use App\FirmDetail;
 use Illuminate\Database\Seeder;
 
 class FirmTypesSeeder extends Seeder
