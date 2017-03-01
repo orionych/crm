c9d80a332d2ac88a5d466ab4bf13a563cde0db4b
= <=>
2017-03-01 13:54:40 +0300

    Auto-committing 'Profile.vue'

--- resources/assets/js/components/Profile.vue
+++ resources/assets/js/components/Profile.vue
@@ -2,5 +2,9 @@
 	<div class='row content-panel'>
 		<div class='col-lg-4 profile-text mt mb centered'>
 		</div>
+		<div class='col-lg-4 profile-text mt mb centered'>
+		</div>
+		<div class='col-lg-4 profile-text mt mb centered'>
+		</div>
 	</div>
 </template>
