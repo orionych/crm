01d1bff695af5ede9d12c0801eec09707706e036
= <=>
2017-03-11 09:14:02 +0300

    Auto-committing 'Price-list.vue'

--- resources/assets/js/components/Price-list.vue
+++ resources/assets/js/components/Price-list.vue
@@ -2,7 +2,7 @@
     <div class="container" style='width:100%'>
         <div class="row">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Прайс-лист: <input type='text' v-model.trim='code'  /> {{ code }} - {{ state }}</div>
+                    <div class="panel-heading">Прайс-лист: <input type='text' v-model.trim='searchQuery'  /> {{ code }} - {{ state }}</div>
                     <div class="panel-body">
                          <div class="row">
                          here new values
@@ -21,6 +21,9 @@
             ready() {
             console.log('Price-list ready.')
         },
+        watch() {
+            searchQuery:
+        }
         mounted() {
             console.log('Component price-list mounted.')
         }
