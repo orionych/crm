4d8a4b711db40c193b6425ede7510634c5df5fba
= <=>
2017-03-11 16:32:10 +0300

    Auto-committing 'EquipmentsController.php'

--- app/Http/Controllers/EquipmentsController.php
+++ app/Http/Controllers/EquipmentsController.php
@@ -10,9 +10,7 @@ class EquipmentsController extends Controller
     public function index(Request $request) {
     	$code=$request->input('code');
     	$equipments=Equipment::with(array('equipment_names'=>function ($query) use ($code) {
-    		return $query->where('equipment_names.name','like','%'.$code.'%');
-    		
-    	}))->limit(50)->get();
+    		return $query->where('equipment_names.name','like','%'.$code.'%');}))->limit(50)->get();
     	return $equipments;
     }
 }
