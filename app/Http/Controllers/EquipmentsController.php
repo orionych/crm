c7c6d7d594e3c54dbef3181141fcdaf12c8e2fe4
= <=>
2017-03-11 13:05:59 +0300

    Auto-committing 'EquipmentsController.php'

--- app/Http/Controllers/EquipmentsController.php
+++ app/Http/Controllers/EquipmentsController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class EquipmentsController extends Controller
 {
     public function index(Request $request) {
-    	$equipments=Equipment::
+    	$equipments=Equipment::where('code','like','%v2fs%');
+    	
     	return $equipments;
     }
 }
