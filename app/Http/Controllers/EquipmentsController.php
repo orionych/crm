19933a5f8c784665066c3385de68e0df2fcf3df2
= <=>
2017-03-11 13:54:28 +0300

    Auto-committing 'EquipmentsController.php'

--- app/Http/Controllers/EquipmentsController.php
+++ app/Http/Controllers/EquipmentsController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class EquipmentsController extends Controller
 {
     public function index(Request $request) {
-    	$equipments=Equipment::with('equipment_names')->where('text','like','%v2fs%')->limit(50)->get();
+    	$equipments=Equipment::with('equipment_names')->where('text','like','%'.$request->input('code').'%')->limit(50)->get();
     	return $equipments;
     }
 }
