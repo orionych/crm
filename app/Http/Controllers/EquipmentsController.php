5244c48063b9557a918d61e4d9b62d1489c067a9
= <=>
2017-03-11 13:02:02 +0300

    Auto-committing 'EquipmentsController.php'

--- app/Http/Controllers/EquipmentsController.php
+++ app/Http/Controllers/EquipmentsController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class EquipmentsController extends Controller
 {
     public function index(Request $request) {
+    	$request->code='new1';
     	return $request->all();
     }
 }
