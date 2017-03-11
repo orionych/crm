45103fbead7f4e1f3c9a9d4d675a6f0faa9d398d
= <=>
2017-03-11 09:04:14 +0300

    Auto-committing 'Price-list.vue'

--- resources/assets/js/components/Price-list.vue
+++ resources/assets/js/components/Price-list.vue
@@ -2,7 +2,7 @@
     <div class="container" style='width:100%'>
         <div class="row">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Прайс-лист: <input type='text' v-model='code' @keyup='onInput' /> {{ state }}</div>
+                    <div class="panel-heading">Прайс-лист: <input type='text' v-model='code'  /> {{ state }}</div>
                     <div class="panel-body">
                          <div class="row">
                          here new values
@@ -21,9 +21,7 @@
             ready() {
             console.log('Price-list ready.')
         },
-        methods: {
-            onInput:debounce(function() {state='loading';},500);
-        },
+
         mounted() {
             console.log('Component price-list mounted.')
         }
