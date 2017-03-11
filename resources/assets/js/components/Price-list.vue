d96a23a36633fff8f57f904e893945ceeab00b90
= <=>
2017-03-11 12:50:05 +0300

    Auto-committing 'Price-list.vue'

--- resources/assets/js/components/Price-list.vue
+++ resources/assets/js/components/Price-list.vue
@@ -25,7 +25,7 @@
             priceLoad: _.debounce(function() {
                 this.loading=true;
                 Vue.http.post('/equipment/list',{code: this.code}).then(response => {
-                  console.log(response.data);
+                  console.log(response.body.data);
                 }, response => {
                   console.log('error loading price list');
                 });
