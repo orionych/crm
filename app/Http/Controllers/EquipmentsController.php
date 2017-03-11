2964374a8e0be9134d1514321acf46251b82321e
= <=>
2017-03-11 14:17:31 +0300

    Auto-committing 'EquipmentsController.php'

--- app/Http/Controllers/EquipmentsController.php
+++ app/Http/Controllers/EquipmentsController.php
@@ -8,9 +8,10 @@ use Illuminate\Http\Request;
 class EquipmentsController extends Controller
 {
     public function index(Request $request) {
-    	$equipments=Equipment::with(array('equipment_names'=>function ($query) use ($request) {
-    		$query->where('equipment_names.name','like','%'.$request->input('code').'%');
-    	}))->where('text','like','%'.$request->input('code').'%')->limit(50)->get();
+    	$code=$request->input('code');
+    	$equipments=Equipment::with(array('equipment_names'=>function ($query) use ($code) {
+    		$query->where('equipment_names.name','like','%'.$code.'%');
+    	}))->where('text','like','%'.$code.'%')->limit(50)->get();
     	return $equipments;
     }
 }
