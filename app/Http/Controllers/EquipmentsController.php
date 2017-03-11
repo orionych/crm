824f9f4a428e0d4f3cf45ec8ef7306ebfbbdc0a7
= <=>
2017-03-11 14:15:33 +0300

    Auto-committing 'EquipmentsController.php'

--- app/Http/Controllers/EquipmentsController.php
+++ app/Http/Controllers/EquipmentsController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class EquipmentsController extends Controller
 {
     public function index(Request $request) {
-    	$equipments=Equipment::with(array('equipment_names'=>function ($query,$request) {
+    	$equipments=Equipment::with(array('equipment_names'=>function ($query) {
     		$query->where('equipment_names.name','like','%'.$request->input('code').'%');
     	}))->where('text','like','%'.$request->input('code').'%')->limit(50)->get();
     	return $equipments;
