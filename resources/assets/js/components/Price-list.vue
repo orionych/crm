98a8a5f22b9250a741a2d7cfc8cab834a62fd01b
= <=>
2017-03-11 10:13:06 +0300

    Auto-committing 'Price-list.vue'

--- resources/assets/js/components/Price-list.vue
+++ resources/assets/js/components/Price-list.vue
@@ -2,7 +2,7 @@
     <div class="container" style='width:100%'>
         <div class="row">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Прайс-лист: <input type='text' v-model.trim='code'  />{{ code }} - {{ state }}</div>
+                    <div class="panel-heading">Прайс-лист: <input type='text' v-model.trim='newFind'  />{{ code }} - {{ state }}</div>
                     <div class="panel-body">
                          <div class="row">
                          here new values
