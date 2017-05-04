1dc3691fdb917a91aefef7e44404446c7cb00355
= <=>
2017-05-04 08:52:34 +0300

    Auto-committing 'Basket.vue'

--- resources/assets/js/components/Basket.vue
+++ resources/assets/js/components/Basket.vue
@@ -7,7 +7,7 @@
                          <div class="row">
                             <div class='col-lg-6'>
                                 <my-input placeholder='Название коммерческого предложения' v-model='name'  ></my-input>
-                                <div class='row'>Срок поставки: <input type='text' v-model='weeks' /><input size=3 type='radio' v-model='weeksshift' value='1' /> +1 <input type='radio' v-model='weeksshift' value='2' /> +2 <input type='radio' v-model='weeksshift' value='3' /> +3</div>
+                                <div class='row'>Срок поставки: <input type='text' v-model='weeks' size=3 /><input type='radio' v-model='weeksshift' value='1' /> +1 <input type='radio' v-model='weeksshift' value='2' /> +2 <input type='radio' v-model='weeksshift' value='3' /> +3</div>
                                <div class='row'>Валюта: <select><option value='Евро'>Eur</option><option value='Рубли'>Рубли</option></div>
                             </div>
                              <div class='col-lg-6'>
